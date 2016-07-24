b07b006f4fb193092688e84876b09fdc95bebd9f
Sasank Chilamkurthy <sasankchilamkurthy@gmail.com>
2016-07-24 13:05:31 +0530

    code refactor - created new class

--- train_transliterator.py
+++ train_transliterator.py
@@ -69,88 +69,107 @@ def batch_generator(pairs, en_dict, hi_dict, batch_size, bucket):
         
         yield X,Y
 
-# TODO: Convert to class
-def train_seq2seq():
-    bucket = (16,16)
-    batch_size = 64
-    embedding_dim = 50
-    memory_dim = 100
-    num_layers = 2
 
-    num_epochs = 100
-    learning_rate = 0.05
-    momentum = 0.9
+class Transliterator(object):
+    """docstring for Transliterator"""
+    def __init__(self, en_hi_pairs, bucket=(16,16), embedding_dim=50, 
+                                            memory_dim=100, num_layers=1):
+        """
+        Create tensorflow model for 
+        """
+        # Get data and Remove pairs not fitting in buckets
+        self.en_hi_pairs = en_hi_pairs
+        self.en_hi_pairs = [x for x in en_hi_pairs if len(x[0])+1 < bucket[0] 
+                                                 and len(x[1])+1 < bucket[1]]
+        self.en_chars, self.en_dict = get_char_vocab('latin')
+        self.hi_chars, self.hi_dict = get_char_vocab('devanagari')
 
-    logdir = tempfile.mkdtemp()
-    print("Logging at {}".format(logdir))
+        self.bucket = bucket
+        self.embedding_dim = embedding_dim
+        self.memory_dim = memory_dim
+        self.num_layers = num_layers
 
-    # Get data
-    en_hi_pairs = get_transliteration_pairs('data/Hindi - Word Transliteration Pairs 1.txt')
-    en_hi_pairs = [x for x in en_hi_pairs if len(x[0])+1 < bucket[0] and len(x[1])+1 < bucket[1]]
-    en_chars, en_dict = get_char_vocab('latin')
-    hi_chars, hi_dict = get_char_vocab('devanagari')
-
-    # Create model
-    enc_inp = [tf.placeholder(tf.int32, shape=(None,),
-                        name="inp%i" % t)
-                    for t in range(bucket[0])]
-
-    labels = [tf.placeholder(tf.int32, shape=(None,),
-                        name="labels%i" % t)
-                    for t in range(bucket[1])]
-
-    weights = [tf.ones_like(labels_t, dtype=tf.float32)
-                    for labels_t in labels]
-
-    # Decoder input: prepend some "GO" token and drop the final
-    # token of the encoder input
-    dec_inp = ([GO_ID*tf.ones_like(labels[0], dtype=np.int32, 
-                        name="GO")] + labels[:-1])
+        #create_model creates tensorflow graphs
+        self.create_model()
 
 
-    single_cell = tf.nn.rnn_cell.GRUCell(memory_dim)
-    cell = single_cell#tf.nn.rnn_cell.MultiRNNCell([single_cell] * num_layers)
+    def create_model(self):
+        """
+        Creates tensorflow variables and graph
+        """
+        self.enc_inp = [tf.placeholder(tf.int32, shape=(None,),
+                            name="inp%i" % t)
+                        for t in range(self.bucket[0])]
 
-    # Sequence to sequence model
-    dec_outputs, dec_memory = tf.nn.seq2seq.embedding_rnn_seq2seq(
-        enc_inp, dec_inp, cell, len(en_chars), len(hi_chars) ,embedding_dim)
+        self.labels = [tf.placeholder(tf.int32, shape=(None,),
+                            name="labels%i" % t)
+                        for t in range(self.bucket[1])]
 
-    # loss and optimizer
-    loss = tf.nn.seq2seq.sequence_loss(dec_outputs, labels, weights)
-    tf.scalar_summary("loss", loss)
-    summary_op = tf.merge_all_summaries()
-    optimizer = tf.train.MomentumOptimizer(learning_rate, momentum)
-    train_op = optimizer.minimize(loss)
+        self.weights = [tf.ones_like(labels_t, dtype=tf.float32)
+                        for labels_t in self.labels]
+
+        # Decoder input: prepend some "GO" token and drop the final
+        # token of the encoder input
+        self.dec_inp = ([GO_ID*tf.ones_like(self.labels[0], dtype=np.int32, 
+                            name="GO")] + self.labels[:-1])
 
 
-    # Run!
-    with tf.Session() as sess:
-        sess.run(tf.initialize_all_variables())
-        summary_writer = tf.train.SummaryWriter(logdir, sess.graph)
+        single_cell = tf.nn.rnn_cell.LSTMCell(self.memory_dim)
+        if self.num_layers > 1:
+            self.cell = tf.nn.rnn_cell.MultiRNNCell([single_cell] * self.num_layers)
+        else:
+            self.cell = single_cell
 
-        step = 0
-        for i in range(num_epochs):
-            print("Epoch: ",i)
-            for X, Y in batch_generator(en_hi_pairs, en_dict, hi_dict, batch_size, bucket):
-                feed_dict = {enc_inp[t]: X[t] for t in range(bucket[0])}
-                feed_dict.update({labels[t]: Y[t] for t in range(bucket[1])})
-                
-                _, loss_t, summary = sess.run([train_op, loss, summary_op], feed_dict)        
-                summary_writer.add_summary(summary, step)
-                
-                # Sample the output every few epochs
-                if step%100 == 0:
-                    print("Step {}:".format(step), "Loss: {}".format(loss_t))
-                    outs_soft = sess.run(dec_outputs, feed_dict)
-                    outs = np.array([logits_t.argmax(axis=1) for logits_t in outs_soft])
-                    for j in range(10):
-                        print('\t', ''.join(reversed([en_chars[x] for x in X[:,j] if x>3])) , ' : ', 
-                              ''.join([hi_chars[x] for x in outs[:,j] if x>3]))
-                
-                step = step + 1
-                                    
-            summary_writer.flush()
+        # Sequence to sequence model
+        self.dec_outputs, self.dec_memory = tf.nn.seq2seq.embedding_rnn_seq2seq(
+                        self.enc_inp, self.dec_inp, self.cell, len(self.en_chars), 
+                                        len(self.hi_chars) ,self.embedding_dim)
+
+
+    def train(self, learning_rate=0.05, momentum=0.9, batch_size=64, num_epochs=100):
+        """
+        Trains model
+        """
+        # Create tempdir for logging
+        logdir = tempfile.mkdtemp()
+        print("Logging at {}".format(logdir))
+
+        # loss and optimizer
+        loss = tf.nn.seq2seq.sequence_loss(self.dec_outputs, self.labels, self.weights)
+        tf.scalar_summary("loss", loss)
+        summary_op = tf.merge_all_summaries()
+        optimizer = tf.train.MomentumOptimizer(learning_rate, momentum)
+        train_op = optimizer.minimize(loss)
+
+        # Run!
+        with tf.Session() as sess:
+            sess.run(tf.initialize_all_variables())
+            summary_writer = tf.train.SummaryWriter(logdir, sess.graph)
+
+            step = 0
+            for i in range(num_epochs):
+                print("Epoch: ",i)
+                for X, Y in batch_generator(self.en_hi_pairs, self.en_dict, self.hi_dict, batch_size, self.bucket):
+                    feed_dict = {self.enc_inp[t]: X[t] for t in range(self.bucket[0])}
+                    feed_dict.update({self.labels[t]: Y[t] for t in range(self.bucket[1])})
+                    
+                    _, loss_t, summary = sess.run([train_op, loss, summary_op], feed_dict)        
+                    summary_writer.add_summary(summary, step)
+                    
+                    # Sample the output every few epochs
+                    if step%100 == 0:
+                        print("Step {}:".format(step), "Loss: {}".format(loss_t))
+                        outs_soft = sess.run(self.dec_outputs, feed_dict)
+                        outs = np.array([logits_t.argmax(axis=1) for logits_t in outs_soft])
+                        for j in range(10):
+                            print('\t', ''.join(reversed([self.en_chars[x] for x in X[:,j] if x>3])) , ' : ', 
+                                  ''.join([self.hi_chars[x] for x in outs[:,j] if x>3]))
+                    
+                    step = step + 1
+                                        
+                summary_writer.flush()
 
 
 if __name__ == '__main__':
-    train_seq2seq()
+    trans = Transliterator(get_transliteration_pairs('data/Hindi - Word Transliteration Pairs 1.txt'))
+    trans.train()
